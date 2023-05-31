40405b9ff6a4f3f966bf16203ee14543ec0b3f8b
ngochuyk812 <ngohuyk80169@gmail.com>
2023-06-01 00:34:42 +0700

    v10'

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const ZingMp3 = require("../model/zingmp3");
-const { log } = require("util");
+const AudioLocal = require("../model/audioLocal")
+const fs = require('fs');
+
+
 
 router.get("/search", async (req, res) => {
   let name = req.query.name;
@@ -29,7 +32,14 @@ router.get("/search", async (req, res) => {
             });
           }
           if (subjects.length === result.length) {
-            res.send(subjects.filter((tmp) => tmp.linkStream));
+            subjects = subjects.filter((tmp) => tmp.linkStream)
+            AudioLocal.getAudio((rs)=>{
+              const serverUrl = `http://${req.headers.host}`;
+              rs.forEach((element,index)=>{
+                rs[index] = {...element, linkStream: serverUrl + "/api/stream?link=" + element.path.replace("\\","/")}
+              })
+              res.send([...rs, ...subjects])
+          })
           }
         });
       });
@@ -37,6 +47,16 @@ router.get("/search", async (req, res) => {
       res.send([]);
     }
   });
+});
+router.get('/stream', function(req, res) {
+  let link = req.query.link ||"uploads\\test.mp3"
+  const data = fs.readFileSync(link);
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.end(data);
+});
+router.get('/segment/:segment', function(req, res) {
+	const segmentPath = 'segment-' + req.params.segment + '.ts';
+	fs.createReadStream(segmentPath).pipe(res);
 });
 router.get("/get_all_info_song", async (req, res) => {
   let id = req.query.id;
@@ -174,5 +194,10 @@ router.get('/get_songs_by_id_playlist', (req,res) => {
 router.get("/top", (req, res) => {
   let id = req.query.id;
   ZingMp3.getTop100().then((rs) => res.send(rs));
+});
+router.get("/test", (req, res) => {
+  const data = fs.readFileSync('uploads\\9b699cf382f14a8e9bbd5c202263c641');
+  res.setHeader('Content-Type', 'audio/mpeg');
+  res.end(data);
 });
 module.exports = router;
